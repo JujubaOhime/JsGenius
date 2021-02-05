b2a52f9a9774d55746d87aa814148b0635c8a081
JujubaOhime <ameliadag96@gmail.com>
2021-02-05 10:18:34 -0300

    colocado tratamento de erro ao salvar uma pontuação

--- js/end.js
+++ js/end.js
@@ -14,28 +14,54 @@ $(".form-end-game").submit(function (e) {
         "name": name, "score": score
     }
     data = JSON.stringify(data)
-    if(name.length)
-    $.ajax({
-        url: urlpost,
-        type: "POST",
-        dataType: "json",
-        contentType: "application/xml",
-        cors: true ,
-        crossDomain: true,
-        cache: true,
-        data: data,
-        headers: {
-            "Access-Control-Allow-Credentials": true,
-            "X-Requested-With": "XMLHttpRequest",
-            "Access-Control-Allow-Origin": "https://us-central1-prova-front-letras.cloudfunctions.net/save",
-            "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
-            "Access-Control-Allow-Headers": "Origin, x-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization",
-        },
-        success:function(response){
-            window.location.href = "ranking.html"
-            //console.log((response[0].name))
-        },
-
-    })
+    if(name.length > 1 && isNaN(score) == false){
+        $.ajax({
+            url: urlpost,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/xml",
+            cors: true ,
+            crossDomain: true,
+            cache: true,
+            data: data,
+            headers: {
+                "Access-Control-Allow-Credentials": true,
+                "X-Requested-With": "XMLHttpRequest",
+                "Access-Control-Allow-Origin": "https://us-central1-prova-front-letras.cloudfunctions.net/save",
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
+                "Access-Control-Allow-Headers": "Origin, x-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization",
+            },
+            success:function(response){
+                window.location.href = "ranking.html"
+                //console.log((response[0].name))
+            },
     
-    });
+        })
+    }
+    else if(name.length <= 1 && isNaN(score) == true){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor, escreva seu nome e obtenha uma pontuação',
+          })
+    }
+    else if(name.length <= 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor, escreva seu nome',
+          })
+    }
+    else if(isNaN(score) == true){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor, obtenha uma pontuação',
+          })
+    }
+    
+    
+    
+    
+    
+});
